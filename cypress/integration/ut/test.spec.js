9909aeba4a58b4c7a3da85b4db2c97c4367dfa50
quang <ngocnq@runsystem.net>
2022-04-09 21:40:09 +0700

    init

--- cypress/integration/ut/test.spec.js
+++ cypress/integration/ut/test.spec.js
@@ -1,12 +1,12 @@
 describe('abc', () => {
 
     it('fails with custom message via overwrite of should', () => {
-        // cy.visit('http://123.25.21.145:1405/worker/login?storeId=1');
-        // // cy.get("#comment").should('eq', 1, { message: 'custom error message 1' })
-        //
-        //
-        // //expect  VS actual
-        // expect("lây trong properrties").to.equal("ket qua chạy method test")
+        cy.visit('http://123.25.21.145:1405/worker/login?storeId=1');
+        // cy.get("#comment").should('eq', 1, { message: 'custom error message 1' })
+
+
+        //expect  VS actual
+        expect("lây trong properrties").to.equal("ket qua chạy method test")
     })
 
 });
